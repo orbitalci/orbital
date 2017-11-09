a6d9f8f78dc4ff518d40a4787eae11dbcc8569e7
Jessi Shank <jessishank@level11.com>
2017-11-09 10:17:13 -0800

    changed ConsumeMessages to be a method on the ProtoConsume struct

--- nsqpb/consume.go
+++ nsqpb/consume.go
@@ -8,11 +8,27 @@ import (
     "sync"
 )
 
+// ProtoConsume wraps nsq.Message so that code outside the package can just add a UnmarshalProtoFunc
+// that doesn't require messing with nsq fields. just write a function that unmarshals to your proto object
+// and does work
+// ...put in WORK.
+type ProtoConsume struct {
+    UnmarshalProtoFunc func([]byte) error
+}
+
+// Actual wrapper for UnmarshalProtoFunc --> nsq.HandlerFunc
+func (p *ProtoConsume) NSQProtoConsume(msg *nsq.Message) error {
+    if err := p.UnmarshalProtoFunc(msg.Body); err != nil {
+        ocelog.LogErrField(err).Warn("nsq proto consume error")
+        return err
+    }
+    return nil
+}
+
 // Consume messages on a given topic / channel in NSQ protoconsume's UnmarshalProtoFunc will be added with
 // a wrapper as a handler for the consumer. The ip address of the NSQLookupd instance
 // can be set by the environment variable NSQLOOKUPD_IP, but will default to 127.0.0.1
-func ConsumeMessages(p *ProtoConsume, topicName string, channelName string) error {
-    // todo: change this to be func (p *ProtoConsume) ConsumeMessages(topicName string, channelName string) {}
+func (p *ProtoConsume) ConsumeMessages(topicName string, channelName string) error {
     wg := &sync.WaitGroup{}
     wg.Add(1)
 
@@ -38,22 +54,5 @@ func ConsumeMessages(p *ProtoConsume, topicName string, channelName string) erro
         return err
     }
     wg.Wait()
-    return nil
-}
-
-// ProtoConsume wraps nsq.Message so that code outside the package can just add a UnmarshalProtoFunc
-// that doesn't require messing with nsq fields. just write a function that unmarshals to your proto object
-// and does work
-// ...put in WORK.
-type ProtoConsume struct {
-    UnmarshalProtoFunc func([]byte) error
-}
-
-// Actual wrapper for UnmarshalProtoFunc --> nsq.HandlerFunc
-func (p *ProtoConsume) NSQProtoConsume(msg *nsq.Message) error {
-    if err := p.UnmarshalProtoFunc(msg.Body); err != nil {
-        ocelog.LogErrField(err).Warn("nsq proto consume error")
-        return err
-    }
     return nil
 }
