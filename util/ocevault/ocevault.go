d54767ed9f188f7f5fe91c248915bc5295436003
Jessi Shank <jessishank@level11.com>
2017-11-10 11:12:47 -0800

    changed all to be a ocevault obj with methods on the struct, makes _way_ more sense.

--- util/ocevault/ocevault.go
+++ util/ocevault/ocevault.go
@@ -3,7 +3,7 @@ package ocevault
 import (
 	"errors"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
+	//"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/vault/api"
 	"os"
 )
@@ -27,52 +27,62 @@ var Token = "e57369ad-9419-cc03-9354-fc227b06f795"
 //	Write(path string, data map[string]interface{}) (*api.Secret, error)
 //}
 
+type Ocevault struct {
+	Client	*api.Client
+	Config	*api.Config
+}
+
+
 // NewEnvAuthedClient will set the Client token based on the environment variable `$VAULT_TOKEN`.
-// Will return error if it is not set.
-func NewEnvAuthClient() (*api.Client, error) {
+// Will return error if it is not set. Returns configured ocevault struct
+func NewEnvAuthClient() (*Ocevault, error) {
 	var token string
 	if token = os.Getenv("VAULT_TOKEN"); token == "" {
-		return &api.Client{}, errors.New("$VAULT_TOKEN not set")
+		return &Ocevault{}, errors.New("$VAULT_TOKEN not set")
 	}
 	return NewAuthedClient(token)
 }
 
 // NewAuthedClient will return a client with default configurations and the Token attached to it.
 // Vault URL configured through VAULT_ADDR environment variable.
-func NewAuthedClient(token string) (cli *api.Client, err error) {
-	config := api.DefaultConfig()
-	cli, err = api.NewClient(config)
+func NewAuthedClient(token string) (oce *Ocevault, err error) {
+	oce = &Ocevault{}
+	oce.Config = api.DefaultConfig()
+	oce.Client, err = api.NewClient(oce.Config)
 	if err != nil {
 		return
 	}
-	cli.SetToken(token)
+	oce.Client.SetToken(token)
 	return
 }
 
 // AddUserAuthData will add the values of the data map to the path of the CI user creds
 // CI vault path set off of base path VaultCIPath
-// Expects that the Client is already initialized properly, will error out if not.
-func AddUserAuthData(cli *api.Client, user string, data map[string]interface{}) (*api.Secret, error){
-	return cli.Logical().Write(fmt.Sprintf(VaultCIPath, user), data)
+func (oce *Ocevault) AddUserAuthData(user string, data map[string]interface{}) (*api.Secret, error){
+	return oce.Client.Logical().Write(fmt.Sprintf(VaultCIPath, user), data)
 }
 
 // GetSecretData will return the Data attribute of the secret you get at the path of the CI user creds, ie all the
-// key-value fields that were set on it. Expects that the Client is already initialized properly (has the
-// appropriate token and URL)
-func GetUserAuthData(cli *api.Client, user string) (map[string]interface{}, error) {
-	secret, err := cli.Logical().Read(fmt.Sprintf(VaultCIPath, user))
+// key-value fields that were set on it
+func (oce *Ocevault) GetUserAuthData(user string) (map[string]interface{}, error){
+	secret, err := oce.Client.Logical().Read(fmt.Sprintf(VaultCIPath, user))
 	if err != nil {
-		return make(map[string]interface{}), err
+		return nil, err
 	}
 	return secret.Data, nil
 }
 
-// Test function
-func Do() {
-	cli, err := NewAuthedClient(Token)
-	if err != nil {
-		panic("boooOOoooooOOoooOOOoo")
-	}
-	v, _ := cli.Logical().Read("secret/booboo")
-	spew.Dump(v.Data)
+func (oce *Ocevault) CreateThrowawayToken() (*api.Secret, error) {
+	return nil, nil
 }
+
+//
+//// Test function
+//func Do() {
+//	cli, err := NewAuthedClient(Token)
+//	if err != nil {
+//		panic("boooOOoooooOOoooOOOoo")
+//	}
+//	v, _ := cli.Logical().Read("secret/booboo")
+//	spew.Dump(v.Data)
+//}
