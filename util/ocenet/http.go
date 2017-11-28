40e79e9e27c7bb3e6ddcc3b52d3ba8180bca4f82
Jessi Shank <jessishank@level11.com>
2017-11-27 17:00:43 -0800

    AppHandler interface

--- util/ocenet/http.go
+++ util/ocenet/http.go
@@ -19,3 +19,26 @@ func InitNegroni(appName string, handler http.Handler) (n *negroni.Negroni){
 	return
 }
 
+// for initializing connections / configurations once, then passing it around for lifetime of application
+// Ctx can be anything, just have to set H to be HandleFunc that also takes in the context as first value.
+// in handle func, cast Ctx interface{} to your struct that you initialized in startup so you can access fields
+// for ex:
+// ```
+// appctx := &MyContext{config: "config yay wooo the best"}
+// muxi.Handle("/ws/builds/{hash}", &ocenet.AppContextHandler{appctx, stream}).Methods("GET")
+// ...
+//
+// func stream(ctx interface{}, w http.ResponseWriter, r *http.Request){
+//     a := ctx.(*MyContext)
+//     // do stuff
+// ...
+// ```
+type AppContextHandler struct {
+	Ctx interface{}
+	H func(interface{}, http.ResponseWriter, *http.Request)
+}
+
+
+func (ah *AppContextHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ah.H(ah.Ctx, w, r)
+}
