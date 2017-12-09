98c407163c5de307dde81b5d0eaf238e1d884397
Marianne Feng <mariannefeng@level11.com>
2017-12-08 16:38:19 -0800

    removed unused build

--- protos/build-protos.sh
+++ protos/build-protos.sh
@@ -3,6 +3,4 @@
 # run protobuf compiler on dir up
 cd ..
 
-#protoc --go_out=protos -I=protos/leveler_resources/protos protos/leveler_resources/protos/*.proto
 protoc --go_out=Mpipeline.proto=github.com/shankj3/ocelot/protos/leveler_resources:protos/ -I=protos/ -I=protos/leveler_resources/protos protos/*.proto
-#protoc --go_out=protos -I=protos -I=protos/leveler_resources/protos protos/*.proto
