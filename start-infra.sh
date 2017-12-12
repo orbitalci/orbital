05ca151859fea01217c257951a868d3ab5060796
Marianne Feng <mariannefeng@level11.com>
2017-12-12 10:47:25 -0800

    runs infrastructure in bg

--- start-infra.sh
+++ start-infra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f infra-docker-compose.yml up
+docker-compose -f infra-docker-compose.yml -d up
 # TODO: TJ has some stuff that will unseal vault for us! Perhaps it goes here?
