3683927b9921f7d765d195b8acceea107fd8ad9f
jessi <jessishank@level11.com>
2017-12-13 23:24:21 +0000

    updating docker compose stuff

--- start-infra.sh
+++ start-infra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f infra-docker-compose.yml up
+docker-compose -f infra-docker-compose.yml up -d
 # TODO: TJ has some stuff that will unseal vault for us! Perhaps it goes here?
