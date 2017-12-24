cfb65ab5f7f53f3e24d30ac392606680a1f150e3
jessi <jessishank@level11.com>
2017-12-24 22:13:55 +0000

    woooeee got it to build

---
+++ template/bb_download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# order of arguments: BBTOKEN, BBDOWNLOAD PATH, GIT COMMIT
+# todo: make sure unzip is installed
+# todo: handle sigterm gracefully, after this container should shut down
+
+if [ $# -gt 0 ]; then
+  count=0
+  args=("$@")
+
+  wget --header="Authorization: Bearer ${args[0]}" "${args[1]}/${args[2]}.zip"
+  mkdir ${args[2]}
+
+  codedir=$(unzip ${args[2]}.zip | awk 'NR==3 {print $2}')
+  cp -r ${codedir}. /${args[2]}
+  # cleanup
+  rm -rf ${codedir}
+  rm ${args[2]}.zip
+
+  echo "Finished with downloading source code"
+  sleep infinity
+else
+    echo "no arguments were passed in"
+fi
+
+
+
+
