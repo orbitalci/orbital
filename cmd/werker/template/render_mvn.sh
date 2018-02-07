7ae0ce1f9914cb75b7e93c322a7c8079d75be761
Jessi Shank <jessishank@level11.com>
2018-02-06 17:09:15 -0800

    templates now in cmd/werker because runtime.Caller() will always return the correct directory

---
+++ cmd/werker/template/render_mvn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# order of arguments: MAVEN_SETTINGS_XML
+if [ $# -gt 0 ]; then
+  args=("$@")
+  mvnsettings=${args[0]}
+  if [ ! -z "${mvnsettings}" ]; then
+    mkdir -p ~/.m2/settings.xml
+    echo ${mvnsettings} >> ~/.m2/settings.xml
+  else
+    echo "maven settings variable empty, saving nothing to settings.xml"
+  fi
+else
+    echo "no arguments were passed in"
+fi
