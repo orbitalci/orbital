1e7aef06dcd092e7bf75f9d07e483719095f00a2
Jessi Shank <jessishank@level11.com>
2018-02-06 17:09:51 -0800

    changed getwd to runtime.Caller because itisnt' dependent on where y ou run the command from

--- cmd/werker/template/render_mvn.sh
+++ cmd/werker/template/render_mvn.sh
@@ -5,7 +5,7 @@ if [ $# -gt 0 ]; then
   args=("$@")
   mvnsettings=${args[0]}
   if [ ! -z "${mvnsettings}" ]; then
-    mkdir -p ~/.m2/settings.xml
+    mkdir -p ~/.m2/
     echo ${mvnsettings} >> ~/.m2/settings.xml
   else
     echo "maven settings variable empty, saving nothing to settings.xml"
