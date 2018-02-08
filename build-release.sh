325aa75d73304ac8d60bfb826ce9c2ae0ac35487
Marianne Feng <mariannefeng@level11.com>
2018-02-08 14:16:16 -0800

    changed build command so that it'll build mac binary

--- build-release.sh
+++ build-release.sh
@@ -8,19 +8,14 @@ dep ensure -v
 export ADMIN_HOST=ec2-34-212-13-136.us-west-2.compute.amazonaws.com
 
 echo "building go project"
-# build binary (RIGHT NOW WILL ONLY BUILD FOR MAC)
-env GOOS=linux GOARCH=amd64 go install ./...
+env GOOS=darwin GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
 
 echo "uploading client binary"
 # zip up the client binary
-cd $HOME/go/bin
 zip -r ocelot.zip ocelot
 
 # upload zipped client binary into s3
 aws s3 cp --acl public-read-write --content-disposition attachment ocelot.zip s3://ocelotty/ocelot.zip
-
-# go back to original directory since we're going to build and stuff
-cd -
 
 # This build assumes you have your ssh key added to L11 bitbucket
 # We need ssh keys to clone from the private bitbucket.
