682b58e10a1cd37305d2abce0cb63c79f31c127b
Jessi Shank <jessishank@level11.com>
2018-03-12 12:14:19 -0700

    adding dockr repo

---
+++ util/repo/dockr/docker.go
@@ -0,0 +1,74 @@
+package dockr
+
+import (
+	"bitbucket.org/level11consulting/ocelot/admin/models"
+	"bitbucket.org/level11consulting/ocelot/util/cred"
+	"bitbucket.org/level11consulting/ocelot/util/repo"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+const dockerConfig = `{
+	"auths": {
+			{{ range $name, $url := .RepoUrl }}
+			"{{$url}}": {
+				"auth": "{{.EncodedAuth}}"
+			}
+	},
+	"HttpHeaders": {
+			"User-Agent": "Docker-Client/17.12.0-ce (linux)"
+	}
+}`
+
+
+type auth map[string]string
+
+type dockerConfigJson struct {
+	Auths map[string]auth `json:"auths,omitempty"`
+	HttpHeaders map[string]string `json:"HttpHeaders,omitempty"`
+
+}
+
+// GetDockerConfig will find docker creds associated with accountName in the CVRemoteConfig, and will
+// generate a config.json authentication file for docker. The contents will be returned base64 encoded for
+// easy passing as a command line argument.
+func GetDockerConfig(rc cred.CVRemoteConfig, accountName string) (string, error) {
+	repod := models.NewRepoCreds()
+	credz, err := rc.GetCredAt(fmt.Sprintf(cred.Docker, accountName), false, repod)
+	if err != nil {
+		return "", err
+	}
+	dockerCred, ok := credz[cred.BuildCredKey("docker", accountName)]
+	if !ok {
+		return "", repo.NCErr("no creds found")
+	}
+	casted, ok := dockerCred.(*models.RepoCreds)
+	if !ok {
+		return "", errors.New(fmt.Sprintf("unable to cast to RepoCreds, which just shouldn't happen. Object: %v", dockerCred))
+	}
+	bitz, err := RCtoDockerConfig(casted)
+	if err != nil {
+		return "", err
+	}
+	configEncoded := base64.StdEncoding.EncodeToString(bitz)
+	return configEncoded, err
+}
+
+
+func RCtoDockerConfig(creds *models.RepoCreds) ([]byte, error) {
+	authstring := fmt.Sprintf("%s:%s", creds.Username, creds.Password)
+	b64authstring := base64.StdEncoding.EncodeToString([]byte(authstring))
+	authz := make(map[string]auth)
+	for _, url := range creds.RepoUrl {
+		authz[url] = map[string]string{"auth":b64authstring}
+	}
+	config := &dockerConfigJson{
+		Auths: authz,
+		HttpHeaders: map[string]string{"User-Agent": "Docker-Client/17.12.0-ce (linux)"},
+	}
+	bitz, err := json.Marshal(config)
+	return bitz, err
+}
+
