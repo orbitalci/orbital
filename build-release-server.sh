9d7a1a1effba8d40549c305cafdc7105f3adee18
Jessi Shank <jessishank@level11.com>
2018-04-02 11:53:24 -0700

    removed aws upload from build-release-server because we don't have aws integration yet

--- build-release-server.sh
+++ build-release-server.sh
@@ -21,43 +21,43 @@ fi
 
 
 # on build server, we know the deps are already up to dat
-
-echo "building ocelot client"
-# mac
-env GOOS=darwin GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
-zip -r mac-ocelot.zip ocelot
-rm ocelot
-
-# window
-env GOOS=windows GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
-zip -r windows-ocelot.zip ocelot
-rm ocelot
-
-# linux
-env GOOS=linux GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
-zip -r linux-ocelot.zip ocelot
-rm ocelot
-
-echo "uploading client binary"
-
-# upload zipped client binary to s3
-aws s3 cp --acl public-read-write --content-disposition attachment mac-ocelot.zip s3://ocelotty/mac-ocelot.zip
-aws s3 cp --acl public-read-write --content-disposition attachment windows-ocelot.zip s3://ocelotty/windows-ocelot.zip
-aws s3 cp --acl public-read-write --content-disposition attachment linux-ocelot.zip s3://ocelotty/linux-ocelot.zip
-
-echo "uploading werker's template files"
-cd werker/builder/template
-tar -cvf werker_files.tar *
-
-# upload zipped werker files to s3
-aws s3 cp --acl public-read-write --content-disposition attachment werker_files.tar s3://ocelotty/werker_files.tar
-
-# cleanup the files we created for s3
-rm werker_files.tar
-cd -
-rm mac-ocelot.zip
-rm windows-ocelot.zip
-rm linux-ocelot.zip
+#
+#echo "building ocelot client"
+## mac
+#env GOOS=darwin GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+#zip -r mac-ocelot.zip ocelot
+#rm ocelot
+#
+## window
+#env GOOS=windows GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+#zip -r windows-ocelot.zip ocelot
+#rm ocelot
+#
+## linux
+#env GOOS=linux GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+#zip -r linux-ocelot.zip ocelot
+#rm ocelot
+#
+#echo "uploading client binary"
+#
+## upload zipped client binary to s3
+#aws s3 cp --acl public-read-write --content-disposition attachment mac-ocelot.zip s3://ocelotty/mac-ocelot.zip
+#aws s3 cp --acl public-read-write --content-disposition attachment windows-ocelot.zip s3://ocelotty/windows-ocelot.zip
+#aws s3 cp --acl public-read-write --content-disposition attachment linux-ocelot.zip s3://ocelotty/linux-ocelot.zip
+#
+#echo "uploading werker's template files"
+#cd werker/builder/template
+#tar -cvf werker_files.tar *
+#
+## upload zipped werker files to s3
+#aws s3 cp --acl public-read-write --content-disposition attachment werker_files.tar s3://ocelotty/werker_files.tar
+#
+## cleanup the files we created for s3
+#rm werker_files.tar
+#cd -
+#rm mac-ocelot.zip
+#rm windows-ocelot.zip
+#rm linux-ocelot.zip
 
 docker tag jessishank/ocelot-build:kill.6 ocelot-build
 
