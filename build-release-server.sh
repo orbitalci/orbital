ce4aa118bcb52e1cb73b2b1d40c75666a9e98794
Jessi Shank <jessishank@level11.com>
2018-04-02 10:37:46 -0700

    created a build-release-server script for the build server that doesn't have docker compose and doesn't build the base image

---
+++ build-release-server.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+
+# on build server, we know the deps are already up to dat
+
+echo "building ocelot client"
+# mac
+env GOOS=darwin GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+zip -r mac-ocelot.zip ocelot
+rm ocelot
+
+# window
+env GOOS=windows GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+zip -r windows-ocelot.zip ocelot
+rm ocelot
+
+# linux
+env GOOS=linux GOARCH=amd64 go build -o ocelot cmd/ocelot/main.go
+zip -r linux-ocelot.zip ocelot
+rm ocelot
+
+echo "uploading client binary"
+
+# upload zipped client binary to s3
+aws s3 cp --acl public-read-write --content-disposition attachment mac-ocelot.zip s3://ocelotty/mac-ocelot.zip
+aws s3 cp --acl public-read-write --content-disposition attachment windows-ocelot.zip s3://ocelotty/windows-ocelot.zip
+aws s3 cp --acl public-read-write --content-disposition attachment linux-ocelot.zip s3://ocelotty/linux-ocelot.zip
+
+echo "uploading werker's template files"
+cd werker/builder/template
+tar -cvf werker_files.tar *
+
+# upload zipped werker files to s3
+aws s3 cp --acl public-read-write --content-disposition attachment werker_files.tar s3://ocelotty/werker_files.tar
+
+# cleanup the files we created for s3
+rm werker_files.tar
+cd -
+rm mac-ocelot.zip
+rm windows-ocelot.zip
+rm linux-ocelot.zip
+
+echo "building admin"
+docker build -f admin/Dockerfile -t jessishank/ocelot-admin:latest .
+echo "building werker"
+docker build -f werker/Dockerfile -t jessishank/ocelot-werker:latest .
+echo "building poller"
+docker build -f cmd/poller/Dockerfile -t jessishank/ocelot-poller:latest .
+echo "building hookhandler"
+docker build -f hookhandler/Dockerfile -t jessishank/ocelot-hookhandler:latest .
+
+docker push jessishank/ocelot-admin:latest
+docker push jessishank/ocelot-werker:latest
+docker push jessishank/ocelot-hookhandler:latest
+docker push jessishank/ocelot-poller:latest
+
+echo "finished building for build server."
