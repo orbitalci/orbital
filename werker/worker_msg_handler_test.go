97d89ca973ea1dc3407e28ab74a50f88cabb8be7
Jessi Shank <jessishank@level11.com>
2018-04-06 13:15:19 -0700

    implemnted trigger in build stage

--- werker/worker_msg_handler_test.go
+++ werker/worker_msg_handler_test.go
@@ -1,9 +1,13 @@
 package werker
 
 import (
+	"bitbucket.org/level11consulting/ocelot/util/storage/models"
 	"bytes"
+	"fmt"
 	"testing"
+
 	"bitbucket.org/level11consulting/go-til/test"
+	pb "bitbucket.org/level11consulting/ocelot/protos"
 )
 
 func TestWorkerMsgHandler_WatchForResults(t *testing.T) {
@@ -32,6 +36,46 @@ func TestWorkerMsgHandler_WatchForResults(t *testing.T) {
 			}
 			if wd.hash != trans.Hash {
 				t.Error(test.StrFormatErrors("git hash", wd.hash, trans.Hash))
+			}
+		})
+	}
+}
+
+
+type dummyBuildStage struct {
+	details []*models.StageResult
+}
+func (dbs *dummyBuildStage) AddStageDetail(stageResult *models.StageResult) error {
+	dbs.details = append(dbs.details, stageResult)
+	return nil
+}
+
+func (dbs *dummyBuildStage) RetrieveStageDetail(buildId int64) ([]models.StageResult, error){
+	var srs []models.StageResult
+	for _, i := range dbs.details {
+		srs = append(srs, *i)
+	}
+	return srs, nil
+}
+
+func Test_handleTriggers(t *testing.T) {
+	var triggerData = []struct {
+		branch string
+		shouldSkip bool
+	}{
+		{"boogaloo", false},
+		{"alks;djf", false},
+		{"vibranium", true},
+	}
+	triggers := &pb.Triggers{Branches: []string{"apple", "banana", "quartz", "vibranium"}}
+	stage := &pb.Stage{Env: []string{}, Script: []string{"echo suuuup yooo"}, Name: "testing_triggers", Trigger: triggers}
+
+	store := &dummyBuildStage{details:[]*models.StageResult{}}
+	for ind, wd := range triggerData {
+		t.Run(fmt.Sprintf("%d-trigger", ind), func(t *testing.T){
+			shouldSkip, _ := handleTriggers(wd.branch, 12, store, stage)
+			if wd.shouldSkip != shouldSkip {
+				t.Error(test.GenericStrFormatErrors("should skip", wd.shouldSkip, shouldSkip))
 			}
 		})
 	}
