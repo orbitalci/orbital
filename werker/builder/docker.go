f15ebbcdbaa5ffdf9c249c2fb1c7aa2802e6ffd0
Jessi Shank <jessishank@level11.com>
2018-04-09 10:05:55 -0700

    updated comment

--- werker/builder/docker.go
+++ werker/builder/docker.go
@@ -77,7 +77,7 @@ func (d *Docker) Setup(ctx context.Context, logout chan []byte, dockerIdChan cha
 
 	logout <- []byte(su.GetStageLabel() + "Creating container...")
 
-	//add environment variables that will always be avilable on the machine - GIT_HASH, BUILD_ID
+	//add environment variables that will always be avilable on the machine - GIT_HASH, BUILD_ID, GIT_HASH_SHORT
 	paddedEnvs := []string{fmt.Sprintf("GIT_HASH=%s", werk.CheckoutHash), fmt.Sprintf("BUILD_ID=%d", werk.Id), fmt.Sprintf("GIT_HASH_SHORT=%s", werk.CheckoutHash[:7])}
 	paddedEnvs = append(paddedEnvs, werk.BuildConf.Env...)
 
