a6ad6592f7479b25ac972233392f193a21a0daa9
Jessi Shank <jessishank@level11.com>
2018-04-13 13:33:01 -0700

    added a thing for pushing to the new dev environment for setup-cv, will default to local.. also commented out ocelot-werker from tag&push

--- scripts/setup-cv.sh
+++ scripts/setup-cv.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
-
+set -e
 # this will setup consul + vault for postgres and assumes you've set the VAULT_TOKEN env variable
-consul kv put config/ocelot/storagetype postgres
+if [ "${PATH_PREFIX}" != "" ]; then
+    prefix="${PATH_PREFIX}/"
+fi
+echo "prefix is ${prefix}"
 
-consul kv put config/ocelot/postgres/db postgres
-consul kv put config/ocelot/postgres/location localhost
-consul kv put config/ocelot/postgres/port 5432
-consul kv put config/ocelot/postgres/username postgres
 
-#vault write secret/config/ocelot/postgres clientsecret=mysecretpassword
-vault kv put secret/config/ocelot/postgres clientsecret=mysecretpassword
+locals() {
+    echo "setting  up local"
+    consul kv put ${prefix}config/ocelot/storagetype postgres
+    consul kv put ${prefix}config/ocelot/postgres/db postgres
+    consul kv put ${prefix}config/ocelot/postgres/location localhost
+    consul kv put ${prefix}config/ocelot/postgres/port 5432
+    consul kv put ${prefix}config/ocelot/postgres/username postgres
+    vault write secret/${prefix}config/ocelot/postgres clientsecret=mysecretpassword
+    #vault kv put secret/${prefix}config/ocelot/postgres clientsecret=mysecretpassword
+
+}
+
+if [ "${DEV_K8S}" == "" ]; then
+    locals
+fi
+
+
+dev_k8s() {
+    echo "setting up dev k8s"
+    loc=$(kubectl get svc -n pedev pgsql-postgresql -o yaml -o jsonpath='{.spec.externalIPs[0]}')
+    port=$(kubectl get svc -n pedev pgsql-postgresql -o yaml -o jsonpath='{.spec.ports[0].port}')
+    pw=$(kubectl get secret --namespace pedev pgsql-postgresql -o jsonpath="{.data.postgres-password}" | base64 --decode; echo)
+    consul kv put ${prefix}config/ocelot/storagetype postgres
+    consul kv put ${prefix}config/ocelot/postgres/db ocelot
+    consul kv put ${prefix}config/ocelot/postgres/location ${loc}
+    consul kv put ${prefix}config/ocelot/postgres/port ${port}
+    consul kv put ${prefix}config/ocelot/postgres/username ocelot
+    vault write secret/${prefix}config/ocelot/postgres clientsecret=${pw}
+    #vault kv put secret/${prefix}config/ocelot/postgres clientsecret=${pw}
+}
+
+if [ "${DEV_K8S}" != "" ]; then
+    dev_k8s
+fi
--- scripts/tag_and_push.sh
+++ scripts/tag_and_push.sh
@@ -5,13 +5,13 @@ if [ $# -eq 1 ]; then
   version=$1
   echo "tagging"
   docker tag ocelot-poller docker.metaverse.l11.com/ocelot-poller:${version}
-  docker tag ocelot-werker docker.metaverse.l11.com/ocelot-werker:${version}
+#  docker tag ocelot-werker docker.metaverse.l11.com/ocelot-werker:${version}
   docker tag ocelot-hookhandler docker.metaverse.l11.com/ocelot-hookhandler:${version}
   docker tag ocelot-admin docker.metaverse.l11.com/ocelot-admin:${version}
 #  docker tag ocelot-build docker.metaverse.l11.com/ocelot-build:${version}
   echo "pushing"
   docker push docker.metaverse.l11.com/ocelot-poller:${version}
-  docker push docker.metaverse.l11.com/ocelot-werker:${version}
+#  docker push docker.metaverse.l11.com/ocelot-werker:${version}
   docker push docker.metaverse.l11.com/ocelot-hookhandler:${version}
   docker push docker.metaverse.l11.com/ocelot-admin:${version}
 #  docker push docker.metaverse.l11.com/ocelot-build:${version}
