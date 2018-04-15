2179d9acc15c25f8a0f64a501b15fefd4c80734e
Jessi Shank <jessishank@level11.com>
2018-04-14 18:15:53 -0700

    got rid of debug line that printed secrets

--- common/credentials/remoteconfig.go
+++ common/credentials/remoteconfig.go
@@ -200,7 +200,6 @@ func (rc *RemoteConfig) GetPassword(scType pb.SubCredType, acctName string, ocyC
 		return "", err
 	}
 	secretData := authData["data"]
-	fmt.Println(secretData)
 	password, ok := secretData.(map[string]interface{})
 	if !ok {
 		return "", errors.New("Could not retrieve password from vault") //how is it that we can't cast to a map of string/string??
