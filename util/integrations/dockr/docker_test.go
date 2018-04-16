cd708e54c2a224cc3d8fb34192e8cf21787e6926
Jessi Shank <jessishank@level11.com>
2018-04-16 10:31:30 -0700

    fixed test

--- util/integrations/dockr/docker_test.go
+++ util/integrations/dockr/docker_test.go
@@ -2,23 +2,31 @@ package dockr
 
 import (
 	"bitbucket.org/level11consulting/ocelot/admin/models"
-	"bytes"
 	//"encoding/base64"
 	//"fmt"
+	"bytes"
 	"testing"
 )
 
 func Test_RCtoDockerConfig(t *testing.T) {
-	repo := &models.RepoCreds{
-		Username: "mysuserisgr8",
-		Password: "apluspassword",
-		RepoUrl: map[string]string{
-			"derpy": "derp.docker.io",
-			"herpy": "herp.docker.io",
+	repos := []models.OcyCredder{&models.RepoCreds{
+		Username:   "mysuserisgr8",
+		Password:   "apluspassword",
+		RepoUrl:    "derp.docker.io",
+		Identifier: "derpy",
+		SubType:    models.SubCredType_DOCKER,
+	},
+		&models.RepoCreds{
+			Username:   "whambam",
+			Password:   "pw1237unsafe",
+			RepoUrl:    "herp.docker.io",
+			Identifier: "herpy",
+			SubType:    models.SubCredType_DOCKER,
 		},
 	}
-	expected := []byte(`{"auths":{"derp.docker.io":{"auth":"bXlzdXNlcmlzZ3I4OmFwbHVzcGFzc3dvcmQ="},"herp.docker.io":{"auth":"bXlzdXNlcmlzZ3I4OmFwbHVzcGFzc3dvcmQ="}},"HttpHeaders":{"User-Agent":"Docker-Client/17.12.0-ce (linux)"}}`)
-	jsonbit, err := RCtoDockerConfig(repo)
+
+	expected := []byte(`{"auths":{"derp.docker.io":{"auth":"bXlzdXNlcmlzZ3I4OmFwbHVzcGFzc3dvcmQ="},"herp.docker.io":{"auth":"d2hhbWJhbTpwdzEyMzd1bnNhZmU="}},"HttpHeaders":{"User-Agent":"Docker-Client/17.12.0-ce (linux)"}}`)
+	jsonbit, err := RCtoDockerConfig(repos)
 	if err != nil {
 		t.Fatal(err)
 	}
