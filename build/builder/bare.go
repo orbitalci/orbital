57f05edc0c749c82013d10e9fa3e19d4ab804f4c
Jessi Shank <jessishank@level11.com>
2018-04-19 12:05:49 -0700

    testing out the run command and stream stdout/stderr

--- build/builder/bare.go
+++ build/builder/bare.go
@@ -13,6 +13,22 @@ import (
 type Host struct {
 
 }
+/*
+what do we need out of bare metal builds? this is pretty much *exclusively* for any ios builds, because you can't run those in a docker container or on kubernetes.
+
+*/
+
+
+func runCommandLogToChan(command *exec.Cmd, logout chan []byte) error{
+	stdout, _ := command.StdoutPipe()
+	stderr, _ := command.StderrPipe()
+	command.Start()
+	//https://stackoverflow.com/questions/45922528/how-to-force-golang-to-close-opened-pipe
+	go streamFromPipe(logout, stdout)
+	go streamFromPipe(logout, stderr)
+	err := command.Wait()
+	return err
+}
 
 func (h *Host) Setup(ctx context.Context, logout chan []byte, dockerIdChan chan string, werk *pb.WerkerTask, rc cred.CVRemoteConfig, werkerPort string) (*pb.Result, string) {
 	cmd := exec.CommandContext(ctx, "sh", "-c", "some long runnig task")
---
+++ build/builder/bare_test.go
@@ -0,0 +1,29 @@
+package builder
+
+import (
+	"context"
+	"os/exec"
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+func Test_runCommandLogToChan(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	logout := make(chan []byte, 1000)
+	cmd := exec.CommandContext(ctx, "/bin/bash", "-c", "echo hi; sleep 2; echo againhi; sleep 1; exit 0")
+	err := runCommandLogToChan(cmd, logout)
+	close(logout)
+	if err != nil {
+		t.Error(err)
+	}
+	expected := []string{"hi", "againhi"}
+	var live []string
+	for i := range logout {
+		live = append(live, string(i))
+	}
+	if diff := deep.Equal(expected, live); diff != nil {
+		t.Error(diff)
+	}
+	cancel()
+}
