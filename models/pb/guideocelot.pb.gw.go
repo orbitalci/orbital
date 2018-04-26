febdfc6abacf8b4844229ddb32edf2534a08ec10
Jessi Shank <jessishank@level11.com>
2018-04-26 10:02:40 -0700

    updating comments and descriptions

--- models/pb/guideocelot.pb.gw.go
+++ models/pb/guideocelot.pb.gw.go
@@ -511,15 +511,6 @@ func request_GuideOcelot_GetK8SCreds_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
-func request_GuideOcelot_GetAllCreds_0(ctx context.Context, marshaler runtime.Marshaler, client GuideOcelotClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq empty.Empty
-	var metadata runtime.ServerMetadata
-
-	msg, err := client.GetAllCreds(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 var (
 	filter_GuideOcelot_BuildRuntime_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
@@ -1302,35 +1293,6 @@ func RegisterGuideOcelotHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
-	mux.Handle("GET", pattern_GuideOcelot_GetAllCreds_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		if cn, ok := w.(http.CloseNotifier); ok {
-			go func(done <-chan struct{}, closed <-chan bool) {
-				select {
-				case <-done:
-				case <-closed:
-					cancel()
-				}
-			}(ctx.Done(), cn.CloseNotify())
-		}
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_GuideOcelot_GetAllCreds_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_GuideOcelot_GetAllCreds_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_GuideOcelot_BuildRuntime_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1688,8 +1650,6 @@ var (
 
 	pattern_GuideOcelot_GetK8SCreds_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "creds", "k8s"}, ""))
 
-	pattern_GuideOcelot_GetAllCreds_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "creds"}, ""))
-
 	pattern_GuideOcelot_BuildRuntime_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "buildruntime"}, ""))
 
 	pattern_GuideOcelot_Logs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "logs"}, ""))
@@ -1747,8 +1707,6 @@ var (
 	forward_GuideOcelot_SetK8SCreds_0 = runtime.ForwardResponseMessage
 
 	forward_GuideOcelot_GetK8SCreds_0 = runtime.ForwardResponseMessage
-
-	forward_GuideOcelot_GetAllCreds_0 = runtime.ForwardResponseMessage
 
 	forward_GuideOcelot_BuildRuntime_0 = runtime.ForwardResponseMessage
 
