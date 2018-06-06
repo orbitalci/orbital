4af9fa0e690a880187b2f8c8683096218aafa49a
Jessi Shank <jessishank1@gmail.com>
2018-06-06 13:31:45 -0700

    changed method name from CheckViability -> ValidateBranchAgainstConf

--- client/validate/validate.go
+++ client/validate/validate.go
@@ -74,7 +74,7 @@ func (c *cmd) validateOcelotYaml(ctx context.Context, ocelotFile string) int {
 		return 1
 	}
 	if c.branch != "" {
-		err := fileValidator.CheckViability(conf, c.branch)
+		err := build.ValidateBranchAgainstConf(conf, c.branch)
 		if err != nil {
 			c.UI.Error("This branch would not build, the validation error was: " + err.Error())
 			return 1
