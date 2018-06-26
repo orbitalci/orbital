2fd8251608d83cff5f8b00d5e53235a4d9ee5a20
Jessi Shank <jessishank1@gmail.com>
2018-06-26 16:03:41 -0700

    fixing bug with admin tests where expecting wrong return number of args, using mocks for testing BuildRepoAndHash

--- router/admin/signaler_actions_test.go
+++ router/admin/signaler_actions_test.go
@@ -5,10 +5,21 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/go-test/deep"
+	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
+	"github.com/shankj3/go-til/consul"
+	"github.com/shankj3/go-til/deserialize"
+	"github.com/shankj3/go-til/nsqpb"
+	"github.com/shankj3/go-til/test"
+	"github.com/shankj3/go-til/vault"
+	"github.com/shankj3/ocelot/build"
+	"github.com/shankj3/ocelot/common/credentials/mock"
 	"github.com/shankj3/ocelot/models"
 	pbb "github.com/shankj3/ocelot/models/bitbucket/pb"
 	"github.com/shankj3/ocelot/models/pb"
+	"github.com/shankj3/ocelot/storage"
+	"google.golang.org/grpc/status"
 )
 
 func TestGuideOcelotServer_WatchRepo(t *testing.T) {
@@ -197,5 +208,200 @@ func (h *handle) CreateWebhook(webhookURL string) error {
 	if h.failHook {
 		return errors.New("failing webhoook")
 	}
+	return nil
+}
+
+var ocelot = []byte(`image: jessishank/mavendocker
+buildTool: maven
+branches:
+- master
+- banana
+env:
+  - "MARIANNE=1"
+stages:
+   - name: inst
+     script:
+        - mvn clean install
+`)
+
+func storageSuccessfulBuild(ocelotStorage *storage.MockOcelotStorage, hash, acct, repo, branch string) {
+	ocelotStorage.EXPECT().AddSumStart(hash, acct, repo, branch).Return(int64(1), nil).Times(1)
+	ocelotStorage.EXPECT().SetQueueTime(int64(1)).Return(nil).Times(1)
+	ocelotStorage.EXPECT().AddStageDetail(gomock.Any()).Return(nil).Times(1)
+}
+
+func TestGuideOcelotServer_BuildRepoAndHash(t *testing.T) {
+	gos, ctl, mockz := setupMockedGuideOcelot(t)
+	defer ctl.Finish()
+	consl := consul.NewMockConsuletty(ctl)
+	vlt := vault.NewMockVaulty(ctl)
+	cred := &pb.VCSCreds{ClientSecret:"1", ClientId: "2", Identifier: "BITBUCKET_shankj3", AcctName:"shankj3", SubType: pb.SubCredType_BITBUCKET, TokenURL: "http://token"}
+	// first, test when there is no hash
+	t.Run("no hash build test", func(t *testing.T){
+		// set up expected mock data
+		mockz.rc.EXPECT().GetConsul().Return(consl).Times(1)
+		mockz.rc.EXPECT().GetVault().Return(vlt).Times(1)
+		mockz.rc.EXPECT().GetCred(mockz.store, pb.SubCredType_BITBUCKET, "BITBUCKET_shankj3", "shankj3", false).Return(cred, nil).Times(1)
+		mockz.handler.EXPECT().GetBranchLastCommitData("shankj3/ocelot", "banana").Return(&pb.BranchHistory{Hash:"123", Branch:"banana"}, nil)
+		mockz.handler.EXPECT().GetFile("ocelot.yml", "shankj3/ocelot", "123").Return(ocelot, nil)
+		consl.EXPECT().GetKeyValue("ci/werker_build_map/123").Return(nil, nil)
+		vlt.EXPECT().CreateThrowawayToken().Return("sup", nil)
+		mockz.producer.EXPECT().WriteProto(gomock.Any(), "build").Times(1)
+		//
+		storageSuccessfulBuild(mockz.store, "123", "shankj3", "ocelot", "banana")
+
+		request := &pb.BuildReq{AcctRepo: "shankj3/ocelot", Branch: "banana"}
+		streamer := &buildserv{}
+		gos.BuildRepoAndHash(request, streamer)
+		expectedLines := []string{
+			"Searching for VCS creds belonging to shankj3/ocelot...",
+			"Successfully found VCS credentials belonging to shankj3/ocelot ✓",
+			"Validating VCS Credentials...",
+			"Successfully used VCS Credentials to obtain a token ✓",
+			"Building branch banana with the latest commit in VCS, which is 123",
+			"Retrieving ocelot.yml for shankj3/ocelot...",
+			"Successfully retrieved ocelot.yml for shankj3/ocelot ✓",
+			"Validating and queuing build data for shankj3/ocelot...",
+			"Build started for 123 belonging to shankj3/ocelot ✓",
+		}
+		if diff := deep.Equal(expectedLines, streamer.lines); diff != nil {
+			t.Error(diff)
+		}
+	})
+
+	// now, test when there is hash
+	t.Run("test building with a branch and a hash", func(t *testing.T){
+		request2 := &pb.BuildReq{AcctRepo: "shankj3/ocelot", Branch: "banana", Hash: "123"}
+		mockz.rc.EXPECT().GetConsul().Return(consl).Times(1)
+		mockz.rc.EXPECT().GetVault().Return(vlt).Times(1)
+		mockz.rc.EXPECT().GetCred(mockz.store, pb.SubCredType_BITBUCKET, "BITBUCKET_shankj3", "shankj3", false).Return(cred, nil).Times(1)
+		mockz.store.EXPECT().RetrieveLatestSum("123").Return(models.BuildSummary{}, storage.BuildSumNotFound("123"))
+		mockz.handler.EXPECT().GetFile("ocelot.yml", "shankj3/ocelot", "123").Return(ocelot, nil)
+		consl.EXPECT().GetKeyValue("ci/werker_build_map/123").Return(nil, nil)
+		vlt.EXPECT().CreateThrowawayToken().Return("sup", nil)
+		mockz.producer.EXPECT().WriteProto(gomock.Any(), "build").Times(1)
+		storageSuccessfulBuild(mockz.store, "123", "shankj3", "ocelot", "banana")
+		streamer := &buildserv{}
+		gos.BuildRepoAndHash(request2, streamer)
+	})
+}
+
+func setupMockedGuideOcelot(t *testing.T) (*guideOcelotServer, *gomock.Controller, *mocks) {
+	ctl := gomock.NewController(t)
+	mockz := &mocks{}
+	mockz.store = storage.NewMockOcelotStorage(ctl)
+	mockz.handler = pb.NewMockVCSHandler(ctl)
+	mockz.rc = mock_credentials.NewMockCVRemoteConfig(ctl)
+	mockz.producer = nsqpb.NewMockProducer(ctl)
+	gos := &guideOcelotServer{
+		Storage: 		mockz.store,
+		RemoteConfig:	mockz.rc,
+		handler:		mockz.handler,
+		Deserializer:	deserialize.New(),
+		Producer:		mockz.producer,
+		OcyValidator:	build.GetOcelotValidator(),
+	}
+	return gos, ctl, mockz
+}
+
+type mocks struct {
+	producer *nsqpb.MockProducer
+	rc *mock_credentials.MockCVRemoteConfig
+	store *storage.MockOcelotStorage
+	handler *pb.MockVCSHandler
+}
+
+func TestGuideOcelotServer_BuildRepoAndHash_previouslybuilt(t *testing.T) {
+	gos, ctl, mockz := setupMockedGuideOcelot(t)
+	defer ctl.Finish()
+	consl := consul.NewMockConsuletty(ctl)
+	vlt := vault.NewMockVaulty(ctl)
+
+	streamer := &buildserv{}
+
+	cred := &pb.VCSCreds{ClientSecret:"1", ClientId: "2", Identifier: "BITBUCKET_shankj3", AcctName:"shankj3", SubType: pb.SubCredType_BITBUCKET, TokenURL: "http://token"}
+
+	request3 := &pb.BuildReq{AcctRepo: "shankj3/ocelot", Hash: "ks72bas"}
+
+	mockz.rc.EXPECT().GetConsul().Return(consl).Times(1)
+	mockz.rc.EXPECT().GetVault().Return(vlt).Times(1)
+	mockz.rc.EXPECT().GetCred(mockz.store, pb.SubCredType_BITBUCKET, "BITBUCKET_shankj3", "shankj3", false).Return(cred, nil).Times(1)
+	mockz.store.EXPECT().RetrieveLatestSum("ks72bas").Return(models.BuildSummary{Hash: "ks72basasdfasdf", Branch: "master", BuildId: 123, Repo: "ocelot", Account: "shankj3"}, nil)
+	mockz.handler.EXPECT().GetFile("ocelot.yml", "shankj3/ocelot", "ks72basasdfasdf").Return(ocelot, nil)
+	consl.EXPECT().GetKeyValue("ci/werker_build_map/ks72basasdfasdf").Return(nil, nil)
+	vlt.EXPECT().CreateThrowawayToken().Return("sup", nil)
+	mockz.producer.EXPECT().WriteProto(gomock.Any(), "build").Times(1)
+	storageSuccessfulBuild(mockz.store, "ks72basasdfasdf", "shankj3", "ocelot", "master")
+	gos.BuildRepoAndHash(request3, streamer)
+	expected := []string{
+		"Searching for VCS creds belonging to shankj3/ocelot...",
+		"Successfully found VCS credentials belonging to shankj3/ocelot ✓",
+		"Validating VCS Credentials...",
+		"Successfully used VCS Credentials to obtain a token ✓",
+		"No branch was passed, using `master` from build #123 instead...",
+		"Found a previous build starting with hash ks72bas, now building branch master ✓",
+		"Retrieving ocelot.yml for shankj3/ocelot...",
+		"Successfully retrieved ocelot.yml for shankj3/ocelot ✓",
+		"Validating and queuing build data for shankj3/ocelot...",
+		"Build started for ks72basasdfasdf belonging to shankj3/ocelot ✓",
+	}
+	if diff := deep.Equal(expected, streamer.lines); diff != nil {
+		t.Error(diff)
+	}
+    // test scenario where hash not found
+	mockz.rc.EXPECT().GetCred(mockz.store, pb.SubCredType_BITBUCKET, "BITBUCKET_shankj3", "shankj3", false).Return(cred, nil).Times(1)
+	mockz.store.EXPECT().RetrieveLatestSum("ks72bas").Return(models.BuildSummary{}, storage.BuildSumNotFound("ks72bas"))
+	request2 := &pb.BuildReq{AcctRepo: "shankj3/ocelot", Hash: "ks72bas"}
+	streamer = &buildserv{}
+	err := gos.BuildRepoAndHash(request2, streamer)
+	expected = []string{
+		"Searching for VCS creds belonging to shankj3/ocelot...",
+		"Successfully found VCS credentials belonging to shankj3/ocelot ✓",
+		"Validating VCS Credentials...",
+		"Successfully used VCS Credentials to obtain a token ✓",
+		"There are no previous builds starting with hash ks72bas...",
+	}
+	stats, _ := status.FromError(err)
+	expectederr := "Branch is a required field if a previous build starting with the specified hash cannot be found. Please pass the branch flag and try again!"
+	if stats.Message() != expectederr {
+		t.Error(test.StrFormatErrors("err msg", expectederr, stats.Message()))
+	}
+	if diff := deep.Equal(expected, streamer.lines); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestGuideOcelotServer_BuildRepoAndHash_branchNotfound(t *testing.T) {
+	gos, ctl, mockz := setupMockedGuideOcelot(t)
+	defer ctl.Finish()
+	req := &pb.BuildReq{AcctRepo: "shankj3/ocelot", Branch: "schmorgazbord"}
+	cred := &pb.VCSCreds{ClientSecret:"1", ClientId: "2", Identifier: "BITBUCKET_shankj3", AcctName:"shankj3", SubType: pb.SubCredType_BITBUCKET, TokenURL: "http://token"}
+	mockz.rc.EXPECT().GetCred(mockz.store, pb.SubCredType_BITBUCKET, "BITBUCKET_shankj3", "shankj3", false).Return(cred, nil).Times(1)
+	mockz.handler.EXPECT().GetBranchLastCommitData("shankj3/ocelot", "schmorgazbord").Return(nil, models.Branch404("schmorgazbord", "shankj3/ocelot")).Times(1)
+	streamer := &buildserv{}
+	errmsg := "Branch schmorgazbord was not found for repository shankj3/ocelot"
+	err := gos.BuildRepoAndHash(req, streamer)
+	statErr, _ := status.FromError(err)
+	if statErr.Message() != errmsg {
+		t.Error(test.StrFormatErrors("err msg", errmsg, statErr.Message()))
+	}
+	expected := []string{
+		"Searching for VCS creds belonging to shankj3/ocelot...",
+		"Successfully found VCS credentials belonging to shankj3/ocelot ✓",
+		"Validating VCS Credentials...",
+		"Successfully used VCS Credentials to obtain a token ✓",
+	}
+	if diff := deep.Equal(expected, streamer.lines); diff != nil {
+		t.Error(diff)
+	}
+}
+
+type buildserv struct {
+	pb.GuideOcelot_BuildRepoAndHashServer
+	lines []string
+}
+
+func (b *buildserv) Send(resp *pb.LineResponse) error {
+	b.lines = append(b.lines, resp.OutputLine)
 	return nil
 }
