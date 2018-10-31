cb171b3aacdd8d9ff35b1b059c1e813e8de5fe4f
Jessi Shank <jessishank1@gmail.com>
2018-10-31 13:59:52 -0500

    updating sql to add id table to credentials and pol table

---
+++ docs/sql/V3__add_credentials_id.sql
@@ -0,0 +1,11 @@
+ALTER TABLE credentials ADD COLUMN id SERIAL;
+ALTER TABLE polling_repos ADD COLUMN credentials_id INT UNIQUE;
+
+UPDATE polling_repos SET (credentials_id) =
+(
+  SELECT id FROM credentials
+  WHERE credentials.account = polling_repos.account
+)
+;
+
+ALTER TABLE polling_repos ALTER COLUMN credentials_id SET NOT NULL;
