1a2970594f6f7d85aee014c07f64ceb8287c071e
Jessi Shank <jessishank1@gmail.com>
2018-11-06 09:12:50 -0800

    updating tests

--- common/du/healthcheck_test.go
+++ common/du/healthcheck_test.go
@@ -21,4 +21,17 @@ func TestHealthChecker_Healthy(t *testing.T) {
 	if err = hc.Healthy(); err == nil {
 		t.Error("it is possible that the machine running htis test has >100000GB of disk space, and this test failure would then be in error. but it feels unlikely.", err)
 	}
+	hc.PauseThreshold = "1q"
+	if err = hc.Healthy(); err == nil {
+		t.Error("invalid threshold, should fail")
+	}
+	hc.PauseThreshold = ""
+	if err = hc.Healthy(); err != nil {
+		t.Error("an empty pausethreshold should result in no checks being made and no error")
+	}
+	hc.PauseThreshold = "1B"
+	hc.PauseThreshold = ""
+	if err = hc.Healthy(); err != nil {
+		t.Error("an empty path should result in no checks being made and no error")
+	}
 }
