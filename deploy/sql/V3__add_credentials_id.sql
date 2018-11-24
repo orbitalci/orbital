30d1feac7e80f5af9e17923af3c9581dd53c6d7e
Jessi Shank <jessishank1@gmail.com>
2018-11-24 11:04:27 -0700

    woops forgot to add cred type

--- deploy/sql/V3__add_credentials_id.sql
+++ deploy/sql/V3__add_credentials_id.sql
@@ -4,7 +4,7 @@ ALTER TABLE polling_repos ADD COLUMN credentials_id INT;
 UPDATE polling_repos SET (credentials_id) =
 (
   SELECT id FROM credentials
-  WHERE credentials.account = polling_repos.account
+  WHERE credentials.account = polling_repos.account AND credentials.cred_type=1
 )
 ;
 
