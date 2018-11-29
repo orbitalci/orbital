375c742664b13a0949a90f391d3afb817ac4edf2
T.J. Telan <tjtelan@level11.com>
2018-11-29 13:18:58 -0800

    go mod tidy updated grpc-gateway to 1.5.1, so changing references in generate.go again, and generated models that we're tracking. Updating go.mod stuff too.

--- models/generate.go
+++ models/generate.go
@@ -1,6 +1,6 @@
 package models
 
-//go:generate protoc --go_out=plugins=grpc:pb/ -I=. -I$GOPATH/src -I${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.4.1/third_party/googleapis/ -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.4.1 --grpc-gateway_out=logtostderr=true:pb --swagger_out=logtostderr=true:pb guideocelot.proto storage.proto build.proto creds.proto vcshandler.proto werkerserver.proto
+//go:generate protoc --go_out=plugins=grpc:pb/ -I=. -I$GOPATH/src -I${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.5.1/third_party/googleapis/ -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.5.1 --grpc-gateway_out=logtostderr=true:pb --swagger_out=logtostderr=true:pb guideocelot.proto storage.proto build.proto creds.proto vcshandler.proto werkerserver.proto
 
 //go:generate protoc-go-inject-tag -input=pb/guideocelot.pb.go
 //go:generate protoc-go-inject-tag -input=pb/build.pb.go
