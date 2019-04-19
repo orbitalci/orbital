ef2d5534c1f41f017c3569c8db608c231396505f
T.J. Telan <tjtelan@level11.com>
2019-04-19 01:01:14 -0700

    Moved build api to build/

--- router/admin/ocelot_server.go
+++ router/admin/ocelot_server.go
@@ -1,14 +1,16 @@
 package admin
 
 import (
+	"github.com/level11consulting/ocelot/build"
 	"github.com/level11consulting/ocelot/repo"
 	"github.com/level11consulting/ocelot/secret"
 	"github.com/level11consulting/ocelot/server/grpc/admin/status"
+
 )
 
 type OcelotServer interface {
 	secret.SecretInterface
 	repo.RepoInterface
 	status.StatusInterface
-	BuildInterface
+	build.BuildInterface
 }
