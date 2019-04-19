fadb9f6fc8d4d0b9a7e2bd13e2c31806c00a0eab
T.J. Telan <tjtelan@level11.com>
2019-04-19 00:22:37 -0700

    Moved repo stuff out of router/admin and into repo/

--- repo/poll/poll_schedule.go
+++ repo/poll/poll_schedule.go
@@ -1,4 +1,4 @@
-package admin
+package poll
 
 import (
 	"context"
--- router/admin/ocelot_server.go
+++ router/admin/ocelot_server.go
@@ -1,12 +1,13 @@
 package admin
 
 import (
+	"github.com/level11consulting/ocelot/repo"
 	"github.com/level11consulting/ocelot/secret"
 )
 
 type OcelotServer interface {
 	secret.SecretInterface
-	RepoInterface
+	repo.RepoInterface
 	StatusInterface
 	BuildInterface
 }
