bd430346114840bf236a9dbf9bcc6fbdd626d81d
T.J. Telan <tjtelan@level11.com>
2019-04-18 23:05:59 -0700

    Adding vcs secret api, and deleting the last traces of the legacy struct. Yay.

--- router/admin/guideocelotstruct.go
+++ router/admin/guideocelotstruct.go
@@ -2,7 +2,6 @@ package admin
 
 import (
 
-	"github.com/level11consulting/ocelot/models"
 	"github.com/shankj3/go-til/deserialize"
 	"github.com/shankj3/go-til/nsqpb"
 
@@ -15,20 +14,7 @@ import (
 )
 
 //this is our grpc server, it responds to client requests
-type guideOcelotServer struct {
-	RemoteConfig   config.CVRemoteConfig
-	Deserializer   *deserialize.Deserializer
-	AdminValidator *validate.AdminValidator
-	RepoValidator  *validate.RepoValidator
-	OcyValidator   *buildconfigvalidator.OcelotValidator
-	Storage        storage.OcelotStorage
-	Producer       nsqpb.Producer
-	handler        models.VCSHandler
-	hhBaseUrl      string
-}
-
 type OcelotServerAPI struct {
-	DeprecatedHandler *guideOcelotServer
 	anycred.AnyCredAPI // This is a hack. Revisit once stable
 	BuildAPI
 	AppleDevSecretAPI
@@ -44,18 +30,6 @@ type OcelotServerAPI struct {
 }
 
 func NewGuideOcelotServer(config config.CVRemoteConfig, d *deserialize.Deserializer, adminV *validate.AdminValidator, repoV *validate.RepoValidator, storage storage.OcelotStorage, hhBaseUrl string) pb.GuideOcelotServer {
-	// changing to this style of instantiation cuz thread safe (idk read it on some best practices, it just looks
-	// purdier to me anyway
-	guideOcelotServer := &guideOcelotServer{
-		OcyValidator:   buildconfigvalidator.GetOcelotValidator(),
-		RemoteConfig:   config,
-		Deserializer:   d,
-		AdminValidator: adminV,
-		RepoValidator:  repoV,
-		Storage:        storage,
-		Producer:       nsqpb.GetInitProducer(),
-		hhBaseUrl:      hhBaseUrl,
-	}
 
 	anyCredAPI := anycred.AnyCredAPI {
 		Storage:        storage,	
@@ -125,7 +99,6 @@ func NewGuideOcelotServer(config config.CVRemoteConfig, d *deserialize.Deseriali
 	}
 
 	return &OcelotServerAPI{ 
-		guideOcelotServer,
 		anyCredAPI,
 		buildAPI,
 		appleDevSecretAPI,
