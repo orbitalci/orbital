95f750e4fb4efc5e3acbe35f24f095cb37b2281f
T.J. Telan <tjtelan@level11.com>
2019-04-18 21:46:52 -0700

    Adding artifact repo api

--- router/admin/artifact_repo_secret.go
+++ router/admin/artifact_repo_secret.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"fmt"
 	"context"
 	"strings"
 
@@ -10,6 +11,9 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"github.com/level11consulting/ocelot/secret"
+	"github.com/level11consulting/ocelot/router/admin/anycred"
+	"github.com/level11consulting/ocelot/server/config"
+	"github.com/level11consulting/ocelot/build/helpers/buildscript/validate"
 )
 
 type ArtifactRepoSecret interface {
@@ -21,9 +25,16 @@ type ArtifactRepoSecret interface {
 	DeleteRepoCreds(context.Context, *pb.RepoCreds) (*empty.Empty, error)
 }
 
-func (g *OcelotServerAPI) GetRepoCreds(ctx context.Context, msg *empty.Empty) (*pb.RepoCredWrapper, error) {
+type ArtifactRepoSecretAPI struct {
+	ArtifactRepoSecret
+	RemoteConfig   config.CVRemoteConfig
+	Storage        storage.OcelotStorage
+	RepoValidator  *validate.RepoValidator
+}
+
+func (g *ArtifactRepoSecretAPI) GetRepoCreds(ctx context.Context, msg *empty.Empty) (*pb.RepoCredWrapper, error) {
 	credWrapper := &pb.RepoCredWrapper{}
-	creds, err := g.DeprecatedHandler.RemoteConfig.GetCredsByType(g.DeprecatedHandler.Storage, pb.CredType_REPO, true)
+	creds, err := g.RemoteConfig.GetCredsByType(g.Storage, pb.CredType_REPO, true)
 
 	if err != nil {
 		if _, ok := err.(*storage.ErrNotFound); !ok {
@@ -41,7 +52,7 @@ func (g *OcelotServerAPI) GetRepoCreds(ctx context.Context, msg *empty.Empty) (*
 	return credWrapper, nil
 }
 
-func (g *OcelotServerAPI) GetRepoCred(ctx context.Context, credentials *pb.RepoCreds) (*pb.RepoCreds, error) {
+func (g *ArtifactRepoSecretAPI) GetRepoCred(ctx context.Context, credentials *pb.RepoCreds) (*pb.RepoCreds, error) {
 	creddy, err := g.GetAnyCred(credentials)
 	if err != nil {
 		return nil, err
@@ -53,15 +64,15 @@ func (g *OcelotServerAPI) GetRepoCred(ctx context.Context, credentials *pb.RepoC
 	return repo, nil
 }
 
-func (g *OcelotServerAPI) SetRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
+func (g *ArtifactRepoSecretAPI) SetRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
 	if creds.SubType.Parent() != pb.CredType_REPO {
 		return nil, status.Error(codes.InvalidArgument, "Subtype must be of repo type: "+strings.Join(pb.CredType_REPO.SubtypesString(), " | "))
 	}
-	err := g.DeprecatedHandler.RepoValidator.ValidateConfig(creds)
+	err := g.RepoValidator.ValidateConfig(creds)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "failed repo creds validation! error: %s", err.Error())
 	}
-	err = secret.SetupRCCCredentials(g.DeprecatedHandler.RemoteConfig, g.DeprecatedHandler.Storage, creds)
+	err = secret.SetupRCCCredentials(g.RemoteConfig, g.Storage, creds)
 	if _, ok := err.(*pb.ValidationErr); ok {
 		return &empty.Empty{}, status.Error(codes.InvalidArgument, "Repo Creds failed validation. Errors are: "+err.Error())
 	}
@@ -71,14 +82,46 @@ func (g *OcelotServerAPI) SetRepoCreds(ctx context.Context, creds *pb.RepoCreds)
 	return &empty.Empty{}, nil
 }
 
-func (g *OcelotServerAPI) UpdateRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
-	return g.UpdateAnyCred(ctx, creds)
+func (g *ArtifactRepoSecretAPI) UpdateRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
+	anyCredAPI := anycred.AnyCredAPI {
+		Storage:        g.Storage,	
+		RemoteConfig:   g.RemoteConfig,
+	}
+
+	return anyCredAPI.UpdateAnyCred(ctx, creds)
 }
 
-func (g *OcelotServerAPI) RepoCredExists(ctx context.Context, creds *pb.RepoCreds) (*pb.Exists, error) {
-	return g.CheckAnyCredExists(ctx, creds)
+func (g *ArtifactRepoSecretAPI) RepoCredExists(ctx context.Context, creds *pb.RepoCreds) (*pb.Exists, error) {
+	anyCredAPI := anycred.AnyCredAPI {
+		Storage:        g.Storage,	
+		RemoteConfig:   g.RemoteConfig,
+	}
+
+	return anyCredAPI.CheckAnyCredExists(ctx, creds)
 }
 
-func (g *OcelotServerAPI) DeleteRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
-	return g.DeleteAnyCred(ctx, creds, pb.CredType_REPO)
+func (g *ArtifactRepoSecretAPI) DeleteRepoCreds(ctx context.Context, creds *pb.RepoCreds) (*empty.Empty, error) {
+	anyCredAPI := anycred.AnyCredAPI {
+		Storage:        g.Storage,	
+		RemoteConfig:   g.RemoteConfig,
+	}
+
+	return anyCredAPI.DeleteAnyCred(ctx, creds, pb.CredType_REPO)
+}
+
+func (g *ArtifactRepoSecretAPI) GetAnyCred(credder pb.OcyCredder) (pb.OcyCredder, error) {
+	if credder.GetSubType() == 0 || credder.GetAcctName() == "" || credder.GetIdentifier() == "" {
+		return nil, status.Error(codes.InvalidArgument, "subType, acctName, and identifier are required fields")
+	}
+	creddy, err := g.RemoteConfig.GetCred(g.Storage, credder.GetSubType(), credder.GetIdentifier(), credder.GetAcctName(), true)
+	if err != nil {
+		if _, ok := err.(*storage.ErrNotFound); ok {
+			return nil, status.Error(codes.NotFound, fmt.Sprintf("Credential %s/%s of Type %s Not Found", credder.GetAcctName(), credder.GetIdentifier(), credder.GetSubType()))
+		}
+		if _, ok := err.(*pb.ValidationErr); ok {
+			return nil, status.Error(codes.InvalidArgument, "Invalid arguments, error: "+err.Error())
+		}
+		return nil, status.Error(codes.Unavailable, "Credential interface not available, error: "+err.Error())
+	}
+	return creddy, nil
 }
