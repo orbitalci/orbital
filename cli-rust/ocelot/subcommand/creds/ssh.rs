cbd508fb6abda18f83bedc2db8d9b9d58b39ed69
T.J. Telan <tjtelan@level11.com>
2019-05-31 16:09:56 -0700

    Adding the rest of the creds subcommands. Renamed notify to slack, and repo to artifact

--- cli-rust/ocelot/subcommand/creds/ssh.rs
+++ cli-rust/ocelot/subcommand/creds/ssh.rs
@@ -1,1 +1,51 @@
 //ocelot creds ssh add --identifier JESSI_SSH_KEY --acctname level11consulting --sshfile-loc /Users/jesseshank/.ssh/id_rsa
+
+extern crate structopt;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab_case")]
+pub struct AddOption {
+    /// Account to add to. Defaults to auto-detect from current working directory
+    #[structopt(name = "Account", long = "acct")]
+    account: Option<String>,
+    /// File path to SSH Private key (type RSA-only)
+    #[structopt(name = "SSH rsa private key", short = "f", long = "file")]
+    file_path: Option<String>,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab_case")]
+pub struct ListOption {
+    /// Account to add to. Defaults to auto-detect from current working directory
+    #[structopt(name = "Account", long = "acct")]
+    account: Option<String>,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab_case")]
+pub enum ResourceAction {
+    /// 
+    Add(AddOption),
+    /// 
+    #[structopt(alias = "rm")]
+    Delete,
+    /// 
+    #[structopt(alias = "ls")]
+    List(ListOption),
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab_case")]
+pub struct SubOption {
+    #[structopt(flatten)]
+    action: ResourceAction,
+
+    #[structopt(name = "Account", long = "acct")]
+    account: Option<String>,
+}
+
+// Handle the command line control flow
+pub fn subcommand_handler(args: &SubOption) {
+    println!("Placeholder for handling SSH creds");
+}
