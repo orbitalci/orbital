e1cdde0b79dbf72d05ad6d3c077d37fcbfd398dd
T.J. Telan <tjtelan@level11.com>
2019-05-31 16:09:56 -0700

    Adding the rest of the creds subcommands. Renamed notify to slack, and repo to artifact

--- cli-rust/ocelot/subcommand/creds/notify.rs
+++
@@ -1,1 +0,0 @@
-//ocelot creds notify add --identifier L11_SLACK --acctname level11consulting --url https://hooks.slack.com/services/T0DFsdSBA/345PPRP9C/5hUe12345v6BrxfSJt --detail-url https://ocelot.mysite.io
