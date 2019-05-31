cfc2b7f57cf8f66fa8d99b6ac3a55a4d4bcc69b0
T.J. Telan <tjtelan@level11.com>
2019-05-31 01:09:24 -0700

    Starting to wire the command line to a subcommand handler using a predictable pattern.

--- cli-rust/ocelot/subcommand/creds/mod.rs
+++ cli-rust/ocelot/subcommand/creds/mod.rs
@@ -6,3 +6,32 @@ pub mod notify;
 pub mod repo;
 pub mod ssh;
 pub mod vcs;
+
+use structopt::StructOpt;
+
+// FIXME: Delete this after we move this into each of the type handlers
+#[derive(Debug, StructOpt, Copy, Clone)]
+#[structopt(rename_all = "kebab_case")]
+pub enum ResourceAction {
+    Add,
+    Delete,
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab_case")]
+pub enum CredType {
+    Apple(apple::AppleOptions),
+    Env(ResourceAction),
+    Helmrepo(ResourceAction),
+    K8s(ResourceAction),
+    Notify(ResourceAction),
+    Repo(ResourceAction),
+    Ssh(ResourceAction),
+    Vcs(vcs::VcsOptions),
+}
+
+// Handle the command line control flow
+pub fn subcommand_handler() {
+    println!("Placeholder for handling creds");
+}
