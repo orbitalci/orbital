3659caced96edcf806ba23a907a0a2d81101c819
T.J. Telan <tjtelan@level11.com>
2019-05-31 01:25:06 -0700

    Got subcommand handling wired deeper into creds

--- cli-rust/ocelot/subcommand/creds/mod.rs
+++ cli-rust/ocelot/subcommand/creds/mod.rs
@@ -32,6 +32,22 @@ pub enum CredType {
 }
 
 // Handle the command line control flow
-pub fn subcommand_handler() {
+pub fn subcommand_handler(args: &CredType) {
     println!("Placeholder for handling creds");
+    println!("{:?}", args);
+
+    match args {
+        CredType::Apple(a) => {
+            apple::subcommand_handler(a);
+        },
+        CredType::Env(a) => {},
+        CredType::Helmrepo(a) => {},
+        CredType::K8s(a) => {},
+        CredType::Notify(a) => {},
+        CredType::Repo(a) => {},
+        CredType::Ssh(a) => {},
+        CredType::Vcs(a) => {
+            vcs::subcommand_handler(a);
+        },
+    }
 }
