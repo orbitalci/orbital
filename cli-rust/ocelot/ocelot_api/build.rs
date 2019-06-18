2072b8e2f335ad7b95fef77d5009ef38b24ac856
T.J. Telan <tjtelan@level11.com>
2019-06-18 10:41:55 -0700

    proof of concept rust talking to our go code. Build still fails bc rust code stream ends early, apparently...

---
+++ cli-rust/ocelot/ocelot_api/build.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+    // Legacy api
+    tower_grpc_build::Config::new()
+        .enable_server(true)
+        .enable_client(true)
+        .build(
+            &[
+                "../../../models/build.proto",
+                "../../../models/creds.proto",
+                "../../../models/guideocelot.proto",
+                "../../../models/storage.proto",
+                "../../../models/vcshandler.proto",
+                // WONTFIX: Single namespace causes conflict w/ prost. We aren't implementing a werker anyway
+                //"../../../models/werkerserver.proto",
+            ],
+            &["../../../models"],
+        )
+        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
+}
