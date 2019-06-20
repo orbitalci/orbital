9c31bd65bbbea865bdd7da33e408e1ddd2de6290
T.J. Telan <tjtelan@level11.com>
2019-06-20 12:43:06 -0700

    Working example of ocelot init

--- cli/ocelot/subcommand/init.rs
+++ cli/ocelot/subcommand/init.rs
@@ -1,6 +1,23 @@
 extern crate structopt;
 use structopt::StructOpt;
 
+use std::env;
+
+use ocelot_api;
+use git_meta::git_info;
+
+use futures::Future;
+use hyper::client::connect::{Destination, HttpConnector};
+use tower_grpc::Request;
+use tower_hyper::{client, util};
+use tower_util::MakeService;
+
+use serde::{Serialize,Deserialize};
+
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab_case")]
 pub struct SubOption {
@@ -11,7 +28,61 @@ pub struct SubOption {
     slack: Option<bool>,
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
+struct OcelotConfigStageTrigger {
+    branches: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
+struct OcelotConfigStage {
+    name: String,
+    trigger: OcelotConfigStageTrigger,
+    script: Vec<String>,
+    env: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
+struct OcelotConfig {
+    version: String,
+    build_tool: String,
+    branches: Vec<String>,
+    env: Vec<String>,
+    stages: Vec<OcelotConfigStage>,
+}
+
 // Handle the command line control flow
 pub fn subcommand_handler(args: &SubOption) {
     println!("Placeholder for handling init");
+
+    let branch_trigger = OcelotConfigStageTrigger{branches: ["master".to_string()].to_vec()};
+    let ocelot_stage = OcelotConfigStage {
+        name: "Test".to_string(),
+        trigger: branch_trigger,
+        env: vec!(),
+        script: ["echo hello world".to_string()].to_vec(),
+    };
+
+    let ocelot_config = OcelotConfig {
+        version: 1.to_string(),
+        build_tool: "docker".to_string(),
+        branches: vec!("master".to_string()),
+        env: vec!(),
+        stages: vec!(ocelot_stage),
+    };
+
+    let mut config_default : OcelotConfig = Default::default();
+    config_default.stages = Vec::new();
+
+    println!("Config: {:?}", serde_yaml::to_string(&ocelot_config));
+
+    match Path::new("./ocelot.yml").exists() {
+        true => {
+            println!("ocelot.yml exists in path. Skipping")
+        }
+        false => {
+            println!("Create ocelot.yml");
+            let mut file = File::create("ocelot.yml").unwrap();
+            file.write_all(serde_yaml::to_string(&ocelot_config).unwrap().as_bytes());
+        }
+    }
 }
