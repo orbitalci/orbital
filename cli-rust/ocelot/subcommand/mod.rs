e0cb74bd07b128a72a64f6c040b14e137b0eefc4
T.J. Telan <tjtelan@level11.com>
2019-06-25 15:35:47 -0700

    Running rustfmt

--- cli-rust/ocelot/subcommand/mod.rs
+++ cli-rust/ocelot/subcommand/mod.rs
@@ -1,13 +1,13 @@
 extern crate structopt;
 
 pub mod build_subcmd;
+pub mod cancel;
 pub mod creds;
 pub mod init;
-pub mod cancel;
 pub mod logs;
 pub mod poll;
 pub mod repo;
+pub mod summary;
 pub mod validate;
 pub mod version;
 pub mod watch;
-pub mod summary;
