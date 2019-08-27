f74ffab5ac1c273b930f7a66a4ab2e8c1bf61a7a
T.J. Telan <tjtelan@level11.com>
2019-08-27 14:37:41 -0700

    Adding the rest of the apis into the orbital api module

--- models/orbital_api/orbital_api.rs
+++ models/orbital_api/orbital_api.rs
@@ -1,3 +1,19 @@
-pub mod legacyapi {
+pub mod build {
     include!(concat!(env!("OUT_DIR"), "/build.rs"));
+}
+
+pub mod credential {
+    include!(concat!(env!("OUT_DIR"), "/credential.rs"));
+}
+
+pub mod integration {
+    include!(concat!(env!("OUT_DIR"), "/integration.rs"));
+}
+
+pub mod organization {
+    include!(concat!(env!("OUT_DIR"), "/organization.rs"));
+}
+
+pub mod state {
+    include!(concat!(env!("OUT_DIR"), "/state.rs"));
 }
