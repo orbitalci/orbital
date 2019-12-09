f5755f6e1b54a39ee148500de1edc2b8e6a5ba5f
T.J. Telan <t.telan@gmail.com>
2019-12-09 00:25:37 -0800

    Adding org database migration

--- models/orbital_database/postgres/schema.rs
+++ models/orbital_database/postgres/schema.rs
@@ -1,47 +1,6 @@
 table! {
     org (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    repo (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    secret (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    notify (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    poll_manifest (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    build_target (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    build_summary (id) {
-        id -> Integer,
-    }
-}
-
-table! {
-    build_stage (id) {
-        id -> Integer,
+        id -> Int4,
+        name -> Varchar,
     }
 }
