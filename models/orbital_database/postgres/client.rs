b5e008b7903b32d617ab25aeb84e5fc221bd6d67
T.J. Telan <t.telan@gmail.com>
2020-01-25 13:13:15 -0800

    Migrating errors in orbital_database to anyhow

--- models/orbital_database/postgres/client.rs
+++ models/orbital_database/postgres/client.rs
@@ -3,6 +3,7 @@ use crate::postgres::repo::{NewRepo, Repo};
 use crate::postgres::schema::{org, repo, secret, SecretType};
 use crate::postgres::secret::{NewSecret, Secret};
 use agent_runtime::vault::orb_vault_path;
+use anyhow::{anyhow, Result};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use log::debug;
@@ -14,7 +15,7 @@ pub fn establish_connection() -> PgConnection {
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
-pub fn org_from_id(conn: &PgConnection, id: i32) -> Result<Org, String> {
+pub fn org_from_id(conn: &PgConnection, id: i32) -> Result<Org> {
     let org_check: Result<Org, _> = org::table
         .select(org::all_columns)
         .filter(org::id.eq(id))
@@ -22,11 +23,11 @@ pub fn org_from_id(conn: &PgConnection, id: i32) -> Result<Org, String> {
 
     match org_check {
         Ok(o) => Ok(o),
-        Err(_e) => Err("Could not retrieve org by id from DB".to_string()),
+        Err(_e) => Err(anyhow!("Could not retrieve org by id from DB")),
     }
 }
 
-pub fn secret_from_id(conn: &PgConnection, id: i32) -> Result<Secret, String> {
+pub fn secret_from_id(conn: &PgConnection, id: i32) -> Result<Secret> {
     let secret_check: Result<Secret, _> = secret::table
         .select(secret::all_columns)
         .filter(secret::id.eq(id))
@@ -34,11 +35,11 @@ pub fn secret_from_id(conn: &PgConnection, id: i32) -> Result<Secret, String> {
 
     match secret_check {
         Ok(o) => Ok(o),
-        Err(_e) => Err("Could not retrieve secret by id from DB".to_string()),
+        Err(_e) => Err(anyhow!("Could not retrieve secret by id from DB")),
     }
 }
 
-pub fn org_add(conn: &PgConnection, name: &str) -> Result<Org, String> {
+pub fn org_add(conn: &PgConnection, name: &str) -> Result<Org> {
     // Only insert if there are no other orgs by this name
     let org_check: Result<Org, _> = org::table
         .select(org::all_columns)
@@ -64,7 +65,7 @@ pub fn org_add(conn: &PgConnection, name: &str) -> Result<Org, String> {
     }
 }
 
-pub fn org_get(conn: &PgConnection, name: &str) -> Result<Org, String> {
+pub fn org_get(conn: &PgConnection, name: &str) -> Result<Org> {
     let mut org_check: Vec<Org> = org::table
         .select(org::all_columns)
         .filter(org::name.eq(&name))
@@ -75,17 +76,17 @@ pub fn org_get(conn: &PgConnection, name: &str) -> Result<Org, String> {
     match &org_check.len() {
         0 => {
             debug!("org not found in db");
-            Err("Org not Found".to_string())
+            Err(anyhow!("Org not Found"))
         }
         1 => {
             debug!("org found in db. Returning result.");
             Ok(org_check.pop().unwrap())
         }
-        _ => panic!("Found more than one org by the same name in db"),
+        _ => Err(anyhow!("Found more than one org by the same name in db")),
     }
 }
 
-pub fn org_update(conn: &PgConnection, name: &str, update_org: NewOrg) -> Result<Org, String> {
+pub fn org_update(conn: &PgConnection, name: &str, update_org: NewOrg) -> Result<Org> {
     let org_update: Org = diesel::update(org::table)
         .filter(org::name.eq(&name))
         .set(update_org)
@@ -97,7 +98,7 @@ pub fn org_update(conn: &PgConnection, name: &str, update_org: NewOrg) -> Result
     Ok(org_update)
 }
 
-pub fn org_remove(conn: &PgConnection, name: &str) -> Result<Org, String> {
+pub fn org_remove(conn: &PgConnection, name: &str) -> Result<Org> {
     let org_delete: Org = diesel::delete(org::table)
         .filter(org::name.eq(&name))
         .get_result(conn)
@@ -106,7 +107,7 @@ pub fn org_remove(conn: &PgConnection, name: &str) -> Result<Org, String> {
     Ok(org_delete)
 }
 
-pub fn org_list(conn: &PgConnection) -> Result<Vec<Org>, String> {
+pub fn org_list(conn: &PgConnection) -> Result<Vec<Org>> {
     let query: Vec<Org> = org::table
         .select(org::all_columns)
         .order_by(org::id)
@@ -121,7 +122,7 @@ pub fn secret_add(
     org: &str,
     name: &str,
     secret_type: SecretType,
-) -> Result<(Secret, Org), String> {
+) -> Result<(Secret, Org)> {
     let query_result: Result<(Secret, Org), _> = secret::table
         .inner_join(org::table)
         .select((secret::all_columns, org::all_columns))
@@ -164,7 +165,7 @@ pub fn secret_get(
     org: &str,
     name: &str,
     _secret_type: SecretType,
-) -> Result<(Secret, Org), String> {
+) -> Result<(Secret, Org)> {
     let query_result: (Secret, Org) = secret::table
         .inner_join(org::table)
         .select((secret::all_columns, org::all_columns))
@@ -183,7 +184,7 @@ pub fn secret_update(
     org: &str,
     name: &str,
     update_secret: NewSecret,
-) -> Result<Secret, String> {
+) -> Result<Secret> {
     let org_db = org_get(conn, org).expect("Unable to find org");
 
     let secret_update: Secret = diesel::update(secret::table)
@@ -203,7 +204,7 @@ pub fn secret_remove(
     org: &str,
     name: &str,
     _secret_type: SecretType,
-) -> Result<Secret, String> {
+) -> Result<Secret> {
     let org_db = org_get(conn, org).expect("Unable to find org");
 
     let secret_delete: Secret = diesel::delete(secret::table)
@@ -219,7 +220,7 @@ pub fn secret_list(
     conn: &PgConnection,
     org: &str,
     filter: Option<SecretType>,
-) -> Result<Vec<(Secret, Org)>, String> {
+) -> Result<Vec<(Secret, Org)>> {
     let query_result: Vec<(Secret, Org)> = match filter {
         None => secret::table
             .inner_join(org::table)
@@ -247,7 +248,7 @@ pub fn repo_add(
     name: &str,
     uri: &str,
     secret: Option<Secret>,
-) -> Result<Repo, String> {
+) -> Result<Repo> {
     let org = org_get(conn, org).expect("Unable to find org");
 
     let secret_id = match secret {
@@ -287,7 +288,7 @@ pub fn repo_add(
     }
 }
 
-pub fn repo_get(conn: &PgConnection, org: &str, name: &str) -> Result<Repo, String> {
+pub fn repo_get(conn: &PgConnection, org: &str, name: &str) -> Result<Repo> {
     debug!("Repo get: Org: {}, Name: {}", org, name);
 
     let org_db = org_get(conn, org).expect("Unable to find org");
@@ -309,7 +310,7 @@ pub fn repo_update(
     org: &str,
     name: &str,
     update_repo: NewRepo,
-) -> Result<Repo, String> {
+) -> Result<Repo> {
     let org_db = org_get(conn, org).expect("Unable to find org");
 
     let repo_update: Repo = diesel::update(repo::table)
@@ -324,7 +325,7 @@ pub fn repo_update(
     Ok(repo_update)
 }
 
-pub fn repo_remove(conn: &PgConnection, org: &str, name: &str) -> Result<Repo, String> {
+pub fn repo_remove(conn: &PgConnection, org: &str, name: &str) -> Result<Repo> {
     let org_db = org_get(conn, org).expect("Unable to find org");
 
     let repo_delete: Repo = diesel::delete(repo::table)
@@ -336,7 +337,7 @@ pub fn repo_remove(conn: &PgConnection, org: &str, name: &str) -> Result<Repo, S
     Ok(repo_delete)
 }
 
-pub fn repo_list(conn: &PgConnection, org: &str) -> Result<Vec<Repo>, String> {
+pub fn repo_list(conn: &PgConnection, org: &str) -> Result<Vec<Repo>> {
     let org_db = org_get(conn, org).expect("Unable to find org");
 
     Ok(repo::table
